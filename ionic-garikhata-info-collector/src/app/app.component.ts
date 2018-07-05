2ce66a0f81ccdb18fed0bbc18d7a497930e0e3cb
Emad Bin Abid <emad.bin.abid@gmail.com>
2018-07-05 20:26:34 +0500

    Added Side Menu.

--- ionic-garikhata-info-collector/src/app/app.component.ts
+++ ionic-garikhata-info-collector/src/app/app.component.ts
@@ -1,22 +1,26 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginProvider } from '../providers/login/login';
+import { LoginRegisterPage } from '../pages/login-register/login-register';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+
   rootPage: any = TabsPage;
+  @ViewChild(Nav) nav: Nav;
 
   constructor(
     platform: Platform, 
     statusBar: StatusBar, 
     splashScreen: SplashScreen,
-    // private loginProvider: LoginProvider
+    private loginProvider: LoginProvider
   ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -29,7 +33,8 @@ export class MyApp {
 
   logoutUser()
   {
-    // this.loginProvider.logout()
+    this.loginProvider.logout()
+    this.nav.push(LoginRegisterPage);
   }
 }
 
