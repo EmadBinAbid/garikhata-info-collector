d9f1c0568226816cd1d0f9b6cc4c86e71803da4d
Emad Bin Abid <emad.bin.abid@gmail.com>
2018-07-05 20:08:04 +0500

    Added alert controllers.

--- ionic-garikhata-info-collector/src/components/register/register.ts
+++ ionic-garikhata-info-collector/src/components/register/register.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { User } from '../../interfaces/user.interface';
 import { UserProvider } from '../../providers/user/user';
+import { AlertController, NavController } from 'ionic-angular';
+import { TabsPage } from '../../pages/tabs/tabs';
 
 /**
  * Generated class for the RegisterComponent component.
@@ -15,35 +17,58 @@ import { UserProvider } from '../../providers/user/user';
 export class RegisterComponent {
 
   registerForm: User = {
-    firstName: undefined,
-    lastName: undefined,
-    username: undefined,
-    email: undefined,
-    password: undefined
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: ""
   };
 
   constructor(
-    private userProvider: UserProvider
+    private userProvider: UserProvider,
+    private alertCtrl: AlertController,
+    private navCtrl: NavController
   ) {
     console.log('Hello RegisterComponent Component');
   }
 
   register()
   {
-    console.log(this.registerForm);
-    this.userProvider.register(this.registerForm)
-    .subscribe( (result: { user: User }) => {
-      if(result.user)
-      {
-        console.log("Registered.");
-        console.log(result);
-      }
-      else
-      {
+    if(this.registerForm.firstName==="" || this.registerForm.lastName==="" || this.registerForm.username==="" ||
+    this.registerForm.email==="" || this.registerForm.password==="")
+    {
+      this.presentAlert('Information Error', 'All fields are required.', ['Ok']);
+    }
+    else
+    {
+      this.userProvider.register(this.registerForm)
+      .subscribe( (result: { user: User }) => {
+        if(result.user)
+        {
+          console.log("Registered.");
+          console.log(result);
 
-      }
-    } );
+          this.presentAlert('Success', 'You have successfully registered to GK Info Collector.', ['Ok']);
+          this.navCtrl.push(TabsPage);
+        }
+      },
+      (err) => {
+        this.presentAlert('Failure', 'Username and email already exist.', ['Ok']);
+      } 
+      );
+    }
+  }
 
+  presentAlert(title: string, subTitle: string, buttons: Array<string>)
+  {
+    let alert = this.alertCtrl.create(
+      {
+        title: title,
+        subTitle: subTitle,
+        buttons: buttons
+      }
+    );
+    alert.present();
   }
 
 }
