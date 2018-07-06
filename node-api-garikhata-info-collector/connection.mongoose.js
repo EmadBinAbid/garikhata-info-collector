4c24c59ad2d2f8d5ead09c0d4eb3cf85d53b5c43
Emad Bin Abid <emad.bin.abid@gmail.com>
2018-07-06 23:23:26 +0500

    Live database deployed.

--- node-api-garikhata-info-collector/connection.mongoose.js
+++ node-api-garikhata-info-collector/connection.mongoose.js
@@ -5,12 +5,15 @@
 
 //Creating database connection
 
-const dbName = "GariKhataInfoCollectorApp";
-const port = "27017";
+// const dbName = "GariKhataInfoCollectorApp";
+// const port = "27017";
+
+const dbName = "@ds229771.mlab.com:29771/gic-garikhatainfocollector";
+const port = "unknown";
 
 exports.connect = function(mongooseInstance)
 {
-    mongooseInstance.connect(`mongodb://localhost/${dbName}`, (err) => {
+    mongooseInstance.connect(`mongodb://emad:emad123@ds229771.mlab.com:29771/gic-garikhatainfocollector`, (err) => {
         if(err)
         {
             console.log(`[-]: Error in MongoDB connection: ${dbName}, PORT: ${port}`);
